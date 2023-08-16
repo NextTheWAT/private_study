46172b064f9f2c439604a4e388e0883bcebd2fff
Lee JaeEun <62467424+NextTheWAT@users.noreply.github.com>
2023-08-16 14:26:24 +0900

    Add files via upload

---
+++ c#_Study/HelloCSharp/HelloCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#");
+            Console.WriteLine("Hellow " + args[0]);
+            Console.WriteLine("args 문자열 갯수 : " + args.Length);
+            Console.ReadKey();  //키를 받을때까지 기다린다.
+        }
+    }
+}
---
+++ c#_Study/HelloCSharpWin/HelloCSharpWin/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloCSharpWin
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloLable_Click(object sender, EventArgs e)
+        {
+            //int number = 10;
+            //string operator1 = "+";
+            //bool isCorrect = true;
+
+            //int sum = 1 + 2;
+
+            int number1 = 1;
+            int number2 = 2;
+
+            int sum = number1 + number2;
+
+            HelloLable.Text = sum.ToString();
+        }
+    }
+}
